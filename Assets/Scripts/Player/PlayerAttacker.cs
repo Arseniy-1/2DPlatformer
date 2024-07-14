0fc9c4e6b8d74c0c487e2999cc16864f449f252b
Darchi <frolova.madina@gmail.com>
2024-07-14 19:28:05 +0300

    Remove Magic Number

--- Assets/Scripts/Player/PlayerAttacker.cs
+++ Assets/Scripts/Player/PlayerAttacker.cs
@@ -5,9 +5,11 @@ public class PlayerAttacker : MonoBehaviour
     [SerializeField] private Transform _attackPoint;
     [SerializeField] private int _damage;
 
+    private float _attackRange = 0.5f;
+
     public void Attack()
     {
-        Collider2D[] hits = Physics2D.OverlapCircleAll(_attackPoint.position, 0.5f);
+        Collider2D[] hits = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange);
 
         foreach(Collider2D hit in hits)
         {
